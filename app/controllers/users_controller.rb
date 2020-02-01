0e6a5c5e3c11c434633a0f93e005ea8dbf3eb6a1
Azraf Chowdhury <achowdhur@ltu.edu>
2020-02-01 12:09:23 +0000

    added login and signup routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,28 @@ class UsersController < ApplicationController
     erb :login
   end
 
+  post '/signup' do
+    if params[:email] == "" || params[:password] == ""
+      redirect 'users/signup'
+    else
+      User.create(email: params[:email], password: params[:password])
+      redirect 'users/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[password])
+      session[:user_id] = @user.id
+      redirect 'users/home'
+    else
+      redirect 'users/login'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect "/"
+  end
+
 end
