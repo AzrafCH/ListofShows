cdc3fbc09a2bd7000c965089016274aaa1470bbe
Azraf Chowdhury <achowdhur@ltu.edu>
2020-02-02 12:21:59 +0000

    no method error for show_url

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -3,7 +3,7 @@ class ShowsController < ApplicationController
   post '/shows' do
     name = params["name"]
     @shows = Show.search_shows_by_name(name)
-
+    @shows_parsed = @shows.order_shows
     erb :index
   end
 
