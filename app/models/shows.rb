8846d06127ce8102eb9772198bce1a14a82c5c65
Azraf Chowdhury <achowdhur@ltu.edu>
2020-02-02 11:23:34 +0000

    added json and http request to show model

--- app/models/shows.rb
+++ app/models/shows.rb
@@ -1,4 +1,25 @@
+require 'json'
+require 'net/http'
+
 class Show < ActiveRecord::Base
   belongs_to :user
   belongs_to :list
+
+  attr_accessor :show_url
+
+  def initialize(url)
+    self.show_url = url
+  end
+
+  def self.search_shows_by_name(keyword)
+    keyword = keyword
+    url = "https://api.themoviedb.org/3/search/tv?api_key=5473abb95c13b13aa72928a042ee4e02&language=en-US&query=#{keyword}"
+    response = self.get_api_response(url)
+  end
+
+  def self.get_api_response(endpoint)
+    uri = URI.parse(URI.encode(endpoint))
+    api_response = Net::HTTP.get(uri)
+    JSON.parse(api_response)
+  end
 end
