4b9f1be2d42b9f262ef167be8e800f9e0489e5f8
Azraf Chowdhury <achowdhur@ltu.edu>
2020-02-05 13:17:12 +0000

    need object ids

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -3,7 +3,6 @@ class ShowsController < ApplicationController
   post '/shows' do
     name = params["name"]
     @shows = Show.search_shows_by_name(name)
-
     erb :index
   end
 
--- app/models/shows.rb
+++ app/models/shows.rb
@@ -6,7 +6,9 @@ class Show < ActiveRecord::Base
   belongs_to :user
   belongs_to :list
 
-  attr_accessor :show_url, :show_id
+  attr_accessor :show_url, :show_id, :show_object
+
+  @filter = {}
 
   def initialize(url)
     self.show_url = url
@@ -17,8 +19,12 @@ class Show < ActiveRecord::Base
     url = "https://api.themoviedb.org/3/search/tv?api_key=5473abb95c13b13aa72928a042ee4e02&language=en-US&query=#{name}"
     response = self.get_api_response(url)
     response["results"].collect do |show|
-      show_url = " " + show["name"] + " " + "|" + " " + show["overview"]
-      Show.new(show_url)
+      show_url = " " + "#{show["name"]}" + " " + "|" + " " + "#{show["overview"]}"
+    #  Show.new(show_url) creates new instance objects, makes list, yet no way of indexing objects
+    #  @filter.merge(show_url) type error, no conversion of string to hash
+    #  @filter[:new_show] = show_url works but renders blanks
+    #  @filter << show_url works, but does not allow me to slice the array, combines all results into one value, cant call upon seperate indexes past 0 as they = nil
+
     end
   end
 
