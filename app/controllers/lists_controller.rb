53772a50c798a6af2ef7a69aa54d14f5e6d902da
Azraf Chowdhury <achowdhur@ltu.edu>
2020-02-06 01:36:27 +0000

    i cant believe i have to monkey patch active record because i would be the only one to get the damn bug

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,8 +1,15 @@
 class ListsController < ApplicationController
 
-  get 'new_list' do
+  post '/new_list' do
     #@shows = Show.all
-    erb :new_list
+    @list = List.create(params[:title])
+    @list.shows << Show.create(name: params["show"]["name"], overview: params["show"]["overview"])
+    redirect '/#{@list.id}'
+  end
+
+  get '/:id' do
+    @list = List.find(params[:id])
+    erb :list_show
   end
 
 end
